03f54a8509fd506666c2be8f9a201fcd412f0353
Msyoka <daviemumo37@gmail.com>
2021-02-08 12:15:21 +0300

    add repo service.ts

--- src/app/repos.service.ts
+++ src/app/repos.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ReposService {
+export class RepoService {
 
-  constructor() { }
+  _URL = 'https://api.github.com/users/';
+  token = 'cbde866d93b44638dbc931242e7f19ea055b30a7';
+
+  constructor(public http: HttpClient) { }
+
+  getRepo(searchTerm: string): Observable<any> {
+    return this.http.get(this._URL + searchTerm + '/repos?' + this.token);
+  }
 }
