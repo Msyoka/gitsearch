65f4f25df1b15a2725a0262fa174349a839a589e
Msyoka <daviemumo37@gmail.com>
2021-02-08 11:57:57 +0300

    add initial repos.ts constructor

--- src/app/mod/repos.ts
+++ src/app/mod/repos.ts
@@ -1,2 +1,11 @@
 export class Repos {
+
+    constructor(
+        public name: any,
+        public description: any,
+        public watchers: any,
+        public language: any,
+        public forks: any
+    ) {}
+    
 }
